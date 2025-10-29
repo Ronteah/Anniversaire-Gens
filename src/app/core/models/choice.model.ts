3f10c6ffcf15eafccd6384ce1ffc4cc5e516f136
Louis <light.louis@hotmail.fr>
2025-10-30 00:17:56 +0100

    Ajout visite guidee dans desc

--- src/app/core/models/choice.model.ts
+++ src/app/core/models/choice.model.ts
@@ -66,7 +66,7 @@ const CIRCUIT_2: Circuit = {
   mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1916.9663952546653!2d-0.8809240746314383!3d47.1970149870463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1761776291677!5m2!1sfr!2sfr',
   imagePath: 'assets/images/vanderhall.png',
   options: [{
-    description: 'Vanderhall Venice',
+    description: 'Visite ludique commentée + Conduire Vanderhall Venice',
     lapsOrTime: '3 km',
   }],
 };
