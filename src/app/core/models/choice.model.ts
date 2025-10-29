0334529d152bc927dc3876cea49344161bc29083
Louis <light.louis@hotmail.fr>
2025-10-29 23:24:27 +0100

    Fix iframe

--- src/app/core/models/choice.model.ts
+++ src/app/core/models/choice.model.ts
@@ -52,7 +52,7 @@ const CIRCUIT_1: Circuit = {
   name: 'EIA Circuits Auto',
   location: 'Pont-l\'Évêque (14)',
   description: 'Ouvert depuis 30 ans, le circuit de Pont l’Évêque est un lieu unique pour vous initier au pilotage, perfectionner votre conduite ou vous reposer dans un cadre se prêtant aux loisirs et à la détente. Situé à proximité de Caen et à environ deux heures de voiture de Paris, ce circuit automobile bénéficie du savoir-faire d’équipes mobilisées 11 mois sur 12 pour vous offrir la meilleure expérience de pilotage dans un environnement exceptionnel.',
-  mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58903.643559018754!2d0.13789003426572538!3d49.272647897734004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e1cde6bda495bb%3A0xb1c66b46b3827711!2sEIA%20Circuits%20Auto%20et%20Karting%20de%20Pont%20l&#39;Ev%C3%AAque!5e0!3m2!1sfr!2sfr!4v1761761413843!5m2!1sfr!2sfr',
+  mapEmbedUrl: `https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d2603.1531448803744!2d0.1909119815091947!3d49.27349527334292!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1761776248438!5m2!1sfr!2sfr`,
   imagePath: 'assets/images/circuit.jpg',
   options: CIRCUIT_1_OPTIONS,
 };
@@ -63,7 +63,7 @@ const CIRCUIT_2: Circuit = {
   name: 'Prestige & Collection',
   location: 'Beaupréau-en-Mauges (49)',
   description: 'Vivez l’expérience de conduire un véhicule atypique : conduite simple, sensation garantie dans ce cabriolet à 3 roues de 180ch. Depuis notre ouverture, il a su faire l’unanimité. Cette conduite de 3km vient sublimé la visite. Pourquoi hésiter, venez l’essayer et le découvrir.',
-  mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2720.672958742548!2d-0.9995536551139454!3d47.23467657158752!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4805ef96e62ff335%3A0x69542a983c749020!2sPrestige%20et%20Collection!5e0!3m2!1sfr!2sfr!4v1700000000000!5m2!1sfr!2sfr',
+  mapEmbedUrl: 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1916.9663952546653!2d-0.8809240746314383!3d47.1970149870463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sfr!2sfr!4v1761776291677!5m2!1sfr!2sfr',
   imagePath: 'assets/images/vanderhall.png',
   options: [{
     description: 'Vanderhall Venice',
